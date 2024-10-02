306a78dc83622009689a18ff989db54feb44e669
Gabriel <gabrielbtcyt@gmail.com>
2024-10-02 16:31:05 -0300

    aula03Exercicios

---
+++ aula03Exercicios/Solucao23.java
@@ -0,0 +1,16 @@
+package aula03Exercicios;
+
+import javax.swing.JOptionPane;
+
+public class Solucao23 {
+    public static void main(String[] args) {
+        int quantidade = Integer.parseInt(JOptionPane.showInputDialog("Digite a quantidade de números pares a imprimir:"));
+        
+        StringBuilder resultado = new StringBuilder();
+        for (int i = 1; i <= quantidade; i++) {
+            resultado.append(i * 2).append(" ");
+        }
+        
+        JOptionPane.showMessageDialog(null, resultado.toString());
+    }
+}
