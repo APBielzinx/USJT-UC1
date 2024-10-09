38908bab01c14e7a990ce29df6bc8f8281e3d3ed
Gabriel <gabrielbtcyt@gmail.com>
2024-10-09 17:51:40 -0300

    aula04Exercicios

--- aula04Exercicios/exemploDisciplina/TesteDisciplina.java
+++ aula04Exercicios/exemploDisciplina/TesteDisciplina.java
@@ -4,17 +4,14 @@ import javax.swing.JOptionPane;
 
 public class TesteDisciplina {
     public static void main(String[] args) {
-        // Ler os dados da disciplina usando JOptionPane
         String nome = JOptionPane.showInputDialog("Digite o nome da disciplina:");
         String professor = JOptionPane.showInputDialog("Digite o nome do professor:");
 
         int semestre = Integer.parseInt(JOptionPane.showInputDialog("Digite o semestre da disciplina:"));
         
-        // Pergunta se a disciplina está sendo ofertada
         int resposta = JOptionPane.showConfirmDialog(null, "A disciplina está sendo ofertada?", "Oferta", JOptionPane.YES_NO_OPTION);
         boolean ofertada = resposta == 0;
 
-        // Instanciar o objeto Disciplina
         Disciplina disciplina = new Disciplina(nome, professor, semestre, ofertada);
 
         JOptionPane.showMessageDialog(null,
